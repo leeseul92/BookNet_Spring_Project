c1a94b2d5ebbee94b47f46bec4486f4a29ac4803
class02 <class02@192.168.0.62>
2020-07-10 15:07:18 +0900

    이명환 200710 정보수정처리 수정

--- BookNet/src/main/java/com/pageturner/cls/dao/ProfileDAO.java
+++ BookNet/src/main/java/com/pageturner/cls/dao/ProfileDAO.java
@@ -3,8 +3,14 @@ package com.pageturner.cls.dao;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.pageturner.cls.vo.ProfileVO;
+
 public class ProfileDAO {
 	@Autowired
 	SqlSessionTemplate sqlSession;
+	
+	public int insertProfile(ProfileVO fVO) {
+		return sqlSession.insert("mSQL.insertProfile", fVO);
+	}
 
 }
